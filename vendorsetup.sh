4f04447a33f605aeffa5636b183fed5ae920b55b
Sushrut1101 <guptasushrut@gmail.com>
2023-08-26 14:29:59 -0300

    vendorsetup.sh: implement based on Galaxy M53

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,66 +1,97 @@
 #
-#	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2019-2022 The OrangeFox Recovery Project
-#	
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
+# Copyright (C) 2023 The OrangeFox Recovery Project
 #
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+# SPDX-License-Identifier: Apache-2.0
 #
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-# 	
-# 	Please maintain this if you use this script or any part of it
-#
+
 FDEVICE="m23xq"
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
-   if [ -n "$chkdev" ]; then
-      FOX_BUILD_DEVICE="$FDEVICE"
-   else
-      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
-      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-   fi
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+	if [ -n "$chkdev" ]; then
+		FOX_BUILD_DEVICE="$FDEVICE"
+	else
+		chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+	fi
 }
 
 if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-   fox_get_target_device
+	fox_get_target_device
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-  export TW_DEFAULT_LANGUAGE="en"
-  export OF_SCREEN_H=2408
-  export OF_CLOCK_POS=1
-	export OF_USE_MAGISKBOOT=1
+	export TW_DEFAULT_LANGUAGE="en"
+	export LC_ALL="C"
+	export ALLOW_MISSING_DEPENDENCIES=true
+
+	export OF_USE_GREEN_LED=0
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_ENABLE_APP_MANAGER=1
+	export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
+	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_FORCE_PREBUILT_KERNEL=1
-	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
-  export LC_ALL="C"
-  export FOX_VANILLA_BUILD=1
-  export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
-  export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=0
-  export OF_MAINTAINER=Aflaungos (mrsiri)
-  export OF_STATUS_H=80
-  export OF_STATUS_INDENT_LEFT=48
-  export OF_STATUS_INDENT_RIGHT=48
-  export OF_ALLOW_DISABLE_NAVBAR=0
-  export OF_CHECK_OVERWRITE_ATTEMPTS=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export OF_NO_MIUI_PATCH_WARNING=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export OF_ENABLE_LPTOOLS=1
+        export OF_QUICK_BACKUP_LIST="/boot;/data;"
+	export OF_PATCH_AVB20=1
+        export FOX_DELETE_AROMAFM=1
+        export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
 
-        # use system (ROM) fingerprint where available
-        # export OF_USE_SYSTEM_FINGERPRINT=1
+	# OTA
+        export OF_VANILLA_BUILD=1
 
-	# OTA for custom ROMs
-        # export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+	# try to prevent potential data format errors
+	export OF_UNBIND_SDCARD_F2FS=1
 
-	# run a process after formatting data to work-around MTP issues (only if forced-encryption is being disabled)
-	# export OF_RUN_POST_FORMAT_PROCESS=1
+	# R11.1 Settings
+	export FOX_VERSION="R11.1"
+
+	# Screen Settings
+	export OF_SCREEN_H=2408
+	export OF_STATUS_H=100
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
+	export OF_CLOCK_POS=1
+	export OF_ALLOW_DISABLE_NAVBAR=0
+
+	# MediaTek
+	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/bootdevice/by-name/recovery"
+	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
+
+	# Magisk
+	function download_magisk(){
+		# Usage: download_magisk <destination_path>
+		local DEST=$1
+		if [ -n "${DEST}" ]; then
+			if [ ! -e ${DEST} ]; then
+				echo "Downloading the Latest Release of Magisk..."
+				local LATEST_MAGISK_URL=$(curl -sL https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep browser_download_url | grep Magisk- | cut -d : -f 2,3 | tr -d '"')
+				mkdir -p $(dirname ${DEST})
+				wget -q ${LATEST_MAGISK_URL} -O ${DEST} || wget ${LATEST_MAGISK_URL} -O ${DEST}
+				local RCODE=$?
+				if [ "$RCODE" = "0" ]; then
+					echo "Successfully Downloaded Magisk to ${DEST}!"
+					echo "Done!"
+				else
+					echo "Failed to Download Magisk to ${DEST}!"
+				fi
+			fi
+		fi
+	}
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
+	download_magisk $FOX_USE_SPECIFIC_MAGISK_ZIP
+
+	# maximum permissible splash image size (in kilobytes); do *NOT* increase!
+	export OF_SPLASH_MAX_SIZE=130
 
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
@@ -69,9 +100,8 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
   	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
  	fi
-
-  	for var in eng user userdebug; do
-  		add_lunch_combo twrp_"$FDEVICE"-$var
-  	done
+else
+	if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+		echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+	fi
 fi
-#
