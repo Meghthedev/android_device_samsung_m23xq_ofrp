21bc6548300055be425ac4beb3f30cd5fb32bfbd
Meghthedev <meghthedev@gmail.com>
2024-01-23 10:07:10 +0530

    treewide: m53x <- m23xq

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),m23xq)
+ifeq ($(TARGET_DEVICE),m53x)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 endif
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,7 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_m23xq.mk
+    $(LOCAL_DIR)/twrp_m53x.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_m23xq-eng
+    twrp_m53x-eng
